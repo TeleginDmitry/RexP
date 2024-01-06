7b87ef0b83886d4a99e1d810acd83fbe45841d0e
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-06 19:30:04 +0300

    #add - inter font

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,8 +1,15 @@
-import { Manrope } from "next/font/google";
+import { Manrope, Inter } from "next/font/google";
 
 export const manrope = Manrope({
   variable: "--font-manrope",
   subsets: ["latin", "cyrillic"],
+  display: "swap",
+  preload: true,
+});
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["cyrillic"],
   display: "swap",
   preload: true,
 });
--- src/components/layout/PageLayout/index.tsx
+++ src/components/layout/PageLayout/index.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 import clsx from "clsx";
 
-import { manrope } from "@/src/assets/fonts/fonts";
+import { inter, manrope } from "@/src/assets/fonts/fonts";
 
 import Footer from "../_components/Footer";
 import Header from "../_components/Header";
@@ -10,7 +10,7 @@ import Header from "../_components/Header";
 import s from "./PageLayout.module.scss";
 
 const PageLayout: React.FC<PropsWithChildren> = ({ children }) => (
-  <div className={clsx(s["page-wrapper"], manrope.variable)}>
+  <div className={clsx(s["page-wrapper"], manrope.variable, inter.variable)}>
     <Header />
     <main className={s["page-layout"]}>{children}</main>
     <Footer />
