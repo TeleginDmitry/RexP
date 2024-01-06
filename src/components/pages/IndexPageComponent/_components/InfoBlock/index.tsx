1ce358a381296c65566ff213d22c92fecdf97217
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-07 02:48:39 +0300

    #add - todo notes

--- src/components/pages/IndexPageComponent/_components/InfoBlock/index.tsx
+++ src/components/pages/IndexPageComponent/_components/InfoBlock/index.tsx
@@ -1,11 +1,14 @@
+import DefaultLink from "@/src/components/ui/links/DefaultLink";
 import RootIcon from "@/src/components/ui/RootIcon";
 import RootText from "@/src/components/ui/RootText";
 
 import s from "./InfoBlock.module.scss";
 
+
+// TODO: add href to links
 const InfoBlock = () => (
   <div className={s.wrapper}>
-    <div className={s.helper}>
+    <DefaultLink className={s.helper} href="/">
       <div className={s.avatar}>Rex</div>
       <div className={s.text}>
         <RootText variant="16px" color="black" className={s["text-top"]}>
@@ -15,8 +18,8 @@ const InfoBlock = () => (
           всегда на связи
         </RootText>
       </div>
-    </div>
-    <div className={s.telegram}>
+    </DefaultLink>
+    <DefaultLink className={s.telegram} href="/">
       <RootIcon name="tg" />
       <div className={s["telegram-text"]}>
         <div className={s["telegram-text-top"]}>@poizonrex</div>
@@ -24,7 +27,7 @@ const InfoBlock = () => (
           с тебя подписка
         </RootText>
       </div>
-    </div>
+    </DefaultLink>
   </div>
 );
 
