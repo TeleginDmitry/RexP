156ff404702c005ee23c6b676e9498ef593d8a87
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-06 19:26:42 +0300

    #add - base profile page

---
+++ pages/profile.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const ProfilePage = () => (
+  <>
+    <Head>
+      <title>title</title>
+      <meta name="description" content="description" />
+    </Head>
+    <div>profile</div>
+  </>
+);
+
+export default ProfilePage;
