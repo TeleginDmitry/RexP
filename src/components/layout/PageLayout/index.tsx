b774c420016e1d021152dbee92af2384740be439
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-06 20:02:34 +0300

    #add - toast messages

--- src/components/layout/PageLayout/index.tsx
+++ src/components/layout/PageLayout/index.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from "react";
 
 import clsx from "clsx";
+import { Toaster } from "sonner";
 
 import { inter, manrope } from "@/src/assets/fonts/fonts";
 
@@ -12,6 +13,7 @@ const PageLayout: React.FC<PropsWithChildren> = ({ children }) => (
   <div className={clsx(s["page-wrapper"], manrope.variable, inter.variable)}>
     <main className={s["page-layout"]}>{children}</main>
     <Footer />
+    <Toaster position="top-center" />
   </div>
 );
 
