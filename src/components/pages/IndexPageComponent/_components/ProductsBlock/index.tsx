bda43cf7e9b0a56c599227e307b09487bcef0beb
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-07 06:13:53 +0300

    #add - mock data

--- src/components/pages/IndexPageComponent/_components/ProductsBlock/index.tsx
+++ src/components/pages/IndexPageComponent/_components/ProductsBlock/index.tsx
@@ -1,3 +1,6 @@
+import photo1 from "@/public/images/mock/photo1.png";
+import photo2 from "@/public/images/mock/photo2.png";
+import photo3 from "@/public/images/mock/photo3.png";
 import CatalogSpacer from "@/src/components/ui/CatalogSpacer";
 import ProductCard from "@/src/components/ui/ProductCard";
 
@@ -8,18 +11,12 @@ const ProductsBlock = () => {
 
   return (
     <CatalogSpacer>
-      <ProductCard
-        price={1233}
-        name="Крокодил Крокодил Крокодил Крокодил КрокодилК рокод илКрокодил Крокодил"
-        imgUrl="/images/indexPage/krokodil.webp"
-        imagePriority
-        id={1}
-      />
-      <ProductCard price={1233} name="Крокодил" imgUrl="/images/indexPage/krokodil.webp" imagePriority id={2} />
-      <ProductCard price={1233} name="Крокодил" imgUrl="/images/indexPage/krokodil.webp" imagePriority id={3} />
-      <ProductCard price={1233} name="Крокодил" imgUrl="/images/indexPage/krokodil.webp" imagePriority id={4} />
-      <ProductCard price={1233} name="Крокодил" imgUrl="/images/indexPage/krokodil.webp" imagePriority id={5} />
-      <ProductCard price={1233} name="Крокодил" imgUrl="/images/indexPage/krokodil.webp" imagePriority id={6} />
+      <ProductCard price={7525} name="adidas originals Samba rose" imgUrl={photo1.src} imagePriority id={1} />
+      <ProductCard price={16684} name="off-white x Converse 1970s" imgUrl={photo1.src} imagePriority id={2} />
+      <ProductCard price={7525} name="adidas originals Samba rose" imgUrl={photo2.src} imagePriority id={3} />
+      <ProductCard price={16684} name="off-white x Converse 1970s" imgUrl={photo3.src} imagePriority id={4} />
+      <ProductCard price={7525} name="adidas originals Samba rose" imgUrl={photo1.src} imagePriority id={5} />
+      <ProductCard price={16684} name="off-white x Converse 1970s" imgUrl={photo2.src} imagePriority id={6} />
     </CatalogSpacer>
   );
 };
