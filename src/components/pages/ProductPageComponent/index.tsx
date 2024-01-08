4ab03b6ca55bf26673b70c30a5eab955daeb65fa
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-08 16:20:09 +0300

    #change - time

--- src/components/pages/ProductPageComponent/index.tsx
+++ src/components/pages/ProductPageComponent/index.tsx
@@ -3,17 +3,17 @@ import { useEffect } from "react";
 import { useLocalStorage } from "@mantine/hooks";
 import { useRouter } from "next/router";
 
-import { PRODUCTS_IN_HISTORY_LS_KEY } from "@/src/constants";
+import { MAX_PRODUCTS_IN_HISTORY, PRODUCTS_IN_HISTORY_LS_KEY } from "@/src/constants";
 import useClientSide from "@/src/hooks/useClientSide";
 
 import AccordionBlock from "./_components/AccordionBlock";
 import AddButton from "./_components/AddButton";
+import InfoBlock from "./_components/InfoBlock";
 import SizesBlock from "./_components/SizesBlock";
 
 import MainContainer from "../../ui/MainContainer";
 
 import s from "./ProductPageComponent.module.scss";
-import InfoBlock from "./_components/InfoBlock";
 
 const ProductPageComponent = () => {
   const router = useRouter();
@@ -32,14 +32,14 @@ const ProductPageComponent = () => {
         }
         const productsId = JSON.parse(historyValue!) as string[];
 
-        if (productsId.filter((id) => id !== router.query.id).length === 6) {
+        if (productsId.filter((id) => id !== router.query.id).length === MAX_PRODUCTS_IN_HISTORY) {
           productsId.pop();
         }
         const newProductsId = [router.query.id, ...productsId.filter((id) => id !== router.query.id)];
         if (JSON.stringify(newProductsId) !== historyValue) {
           setHistoryValue(JSON.stringify(newProductsId));
         }
-      }, 200);
+      }, 150);
     }
   }, [historyValue, isClient, router.query.id]);
 
