b3a0993b56354d74af5d09b586ebae61f2424313
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-08 15:13:46 +0300

    #add - order header

--- src/components/pages/OrderPageComponent/index.tsx
+++ src/components/pages/OrderPageComponent/index.tsx
@@ -1,11 +1,27 @@
 import { useRouter } from "next/router";
 
+import RootIcon from "../../ui/icons/RootIcon";
+import DefaultLink from "../../ui/links/DefaultLink";
+import MainContainer from "../../ui/MainContainer";
+
 import s from "./OrderPageComponent.module.scss";
 
 const OrderPageComponent = () => {
   const router = useRouter();
 
-  return <div className={s[""]}>id: {router.query.id}</div>;
+  return (
+    <MainContainer className={s.wrapper}>
+      <div className={s.header}>
+        <DefaultLink href="/profile/delivery" className={s.link} aria-label="Назад">
+          <RootIcon name="arrowLeft" />
+        </DefaultLink>
+        <h1 className={s.title}>
+          Заказ от 21 ноября
+          <span>№ {router.query.id}</span>
+        </h1>
+      </div>
+    </MainContainer>
+  );
 };
 
 export default OrderPageComponent;
