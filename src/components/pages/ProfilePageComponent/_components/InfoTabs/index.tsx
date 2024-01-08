45c3cd9ff47356ef5d039d2d0d8d2d4c324250b4
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-08 10:06:28 +0300

    #add - info tabs

---
+++ src/components/pages/ProfilePageComponent/_components/InfoTabs/index.tsx
@@ -0,0 +1,34 @@
+import RootLink from "@/src/components/ui/links/RootLink";
+
+import s from "./InfoTabs.module.scss";
+
+const InfoTabs = () => {
+  const count = 1;
+  const deliveryCount = 3;
+
+  return (
+    <div className={s.wrapper}>
+      <RootLink className={s.link} href="/profile/deliveryInfo">
+        <span className={s.text}>
+          Данные
+          <br />
+          доставки
+        </span>
+      </RootLink>
+      <RootLink className={s.link} href="/profile/delivery">
+        <span className={s.count}>{count}</span>
+        <span className={s.text}>Мои заказы</span>
+      </RootLink>
+      <RootLink className={s.link} href="/profile/review">
+        <span className={s.text}>Ждут отзыва</span>
+        <span className={s["delivery-count"]}>{deliveryCount} заказа</span>
+        <span className={s["review-text"]}>
+          Напиши отзыв
+          <br />и получи промокод
+        </span>
+      </RootLink>
+    </div>
+  );
+};
+
+export default InfoTabs;
--- src/components/pages/ProfilePageComponent/index.tsx
+++ src/components/pages/ProfilePageComponent/index.tsx
@@ -1,4 +1,5 @@
 import HistoryProducts from "./_components/HistoryProducts";
+import InfoTabs from "./_components/InfoTabs";
 import UserInfo from "./_components/UserInfo";
 
 import DefaultLink from "../../ui/links/DefaultLink";
@@ -9,6 +10,7 @@ import s from "./ProfilePageComponent.module.scss";
 const ProfilePageComponent = () => (
   <MainContainer className={s.wrapper}>
     <UserInfo />
+    <InfoTabs />
     <HistoryProducts />
     <DefaultLink className={s.info} href="/">
       Юридическая информация
