b2168b328e15f0c1b60c5f932784ecb4461577c1
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-10 03:16:36 +0300

    #add - product card

---
+++ src/components/pages/BasketPageComponent/_components/ProductsBlock/index.tsx
@@ -0,0 +1,68 @@
+import { useLocalStorage } from "@mantine/hooks";
+import { Checkbox, cn } from "@nextui-org/react";
+import clsx from "clsx";
+import { motion } from "framer-motion";
+
+import HeartIcon from "@/src/components/ui/icons/HeartIcon";
+import InViewWrapper from "@/src/components/ui/InViewWrapper";
+import { PRODUCTS, PRODUCTS_IN_BASKET_LS_KEY } from "@/src/constants";
+import useClientSide from "@/src/hooks/useClientSide";
+
+import s from "./ProductsBlock.module.scss";
+
+const ProductsBlock = () => {
+  const isClient = useClientSide();
+  const [basketValue] = useLocalStorage({
+    key: PRODUCTS_IN_BASKET_LS_KEY,
+    defaultValue: "",
+  });
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <div className={s.wrapper}>
+      {basketValue &&
+        PRODUCTS.filter((product) => JSON.parse(basketValue).includes(`${product.id}`)).map(({ id, name }, index) => (
+          <InViewWrapper key={id}>
+            {({ isInView }) => (
+              <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 30 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.3, delay: index < 2 ? 0.1 * index : 0.15 }}
+                className={s["checkbox-wrapper"]}
+              >
+                <Checkbox
+                  aria-label={name}
+                  radius="full"
+                  size="lg"
+                  classNames={{
+                    base: clsx(
+                      cn(
+                        "inline-flex max-w-md w-full m-0",
+                        "hover:bg-content2 items-center justify-start",
+                        "cursor-pointer rounded-[0px] gap-2 p-[0px]  border-transparent",
+                        "data-[selected=true]:border-primary"
+                      ),
+                      s.checkbox
+                    ),
+                    label: "w-[calc(100%_-_74px)]",
+                  }}
+                  value={`${id}`}
+                >
+                  <div className={s.header}>{name}</div>
+                </Checkbox>
+                <div className={s.footer}>
+                  <HeartIcon productId={id} />
+                </div>
+              </motion.div>
+            )}
+          </InViewWrapper>
+        ))}
+    </div>
+  );
+};
+
+export default ProductsBlock;
--- src/components/pages/BasketPageComponent/index.tsx
+++ src/components/pages/BasketPageComponent/index.tsx
@@ -1,31 +1,13 @@
-import { useLocalStorage } from "@mantine/hooks";
-
-import { PRODUCTS_IN_BASKET_LS_KEY } from "@/src/constants";
-import useClientSide from "@/src/hooks/useClientSide";
+import ProductsBlock from "./_components/ProductsBlock";
 
 import MainContainer from "../../ui/MainContainer";
 
 import s from "./BasketPageComponent.module.scss";
 
-const BasketPageComponent = () => {
-  const isClient = useClientSide();
-  const [basketValue] = useLocalStorage({
-    key: PRODUCTS_IN_BASKET_LS_KEY,
-    defaultValue: "",
-  });
-
-  if (!isClient) {
-    return null;
-  }
-
-  return (
-    <MainContainer className={s.wrapper}>
-      в корзине
-      <div className={s[""]}>
-        {basketValue && JSON.parse(basketValue).map((id: string) => <div key={id}> id: {id}</div>)}
-      </div>
-    </MainContainer>
-  );
-};
+const BasketPageComponent = () => (
+  <MainContainer className={s.wrapper}>
+    <ProductsBlock />
+  </MainContainer>
+);
 
 export default BasketPageComponent;
