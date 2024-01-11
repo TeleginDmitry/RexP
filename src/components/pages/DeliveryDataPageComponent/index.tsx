c5ce268868085b2e3172f8016b1a2ea334e43634
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-11 05:46:20 +0300

    #add - styles

--- src/components/pages/DeliveryDataPageComponent/index.tsx
+++ src/components/pages/DeliveryDataPageComponent/index.tsx
@@ -1,11 +1,19 @@
 import AddButton from "./_components/AddButton";
 
+import RootIcon from "../../ui/icons/RootIcon";
+import DefaultLink from "../../ui/links/DefaultLink";
 import MainContainer from "../../ui/MainContainer";
 
 import s from "./DeliveryDataPageComponent.module.scss";
 
 const DeliveryDataPageComponent = () => (
   <MainContainer className={s.wrapper}>
+    <div className={s.header}>
+      <DefaultLink href="/profile" className={s.link} aria-label="Назад">
+        <RootIcon name="arrowLeft" />
+      </DefaultLink>
+      <h1 className={s.title}>Данные доставки</h1>
+    </div>
     <AddButton />
   </MainContainer>
 );
