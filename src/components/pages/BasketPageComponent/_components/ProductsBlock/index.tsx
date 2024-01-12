56bae33ade69ddded1b1d6b9743a3a105915255d
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-12 23:57:29 +0300

    #add

--- src/components/pages/BasketPageComponent/_components/ProductsBlock/index.tsx
+++ src/components/pages/BasketPageComponent/_components/ProductsBlock/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button, Checkbox, CheckboxGroup, cn } from "@nextui-org/react";
 import clsx from "clsx";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 import HeartIcon from "@/src/components/ui/icons/HeartIcon";
 import InViewWrapper from "@/src/components/ui/InViewWrapper";
@@ -20,17 +21,19 @@ const ProductsBlock = () => {
 
   const totalPrice = carts
     .filter((cart) => selected.includes(cart.id.toString()))
-    .reduce((acc, cart) => acc + cart.product.price * cart.count, 0).toFixed(0);
+    .reduce((acc, cart) => acc + cart.product.price * cart.count, 0)
+    .toFixed(0);
   const totalPriceWithDiscount = carts
     .filter((cart) => selected.includes(cart.id.toString()))
-    .reduce((acc, cart) => acc + cart.product.price * cart.count * ((100 - cart.product.discount) / 100), 0).toFixed(0);
+    .reduce((acc, cart) => acc + cart.product.price * cart.count * ((100 - cart.product.discount) / 100), 0)
+    .toFixed(0);
 
   return (
     <>
       <HeaderBlock selected={selected} setSelected={setSelected} />
       <CheckboxGroup className={s.wrapper} onValueChange={setSelected} value={selected}>
         {carts.map(({ id, size, count, product }, index) => (
-          <InViewWrapper key={`${id}${size}`}>
+          <InViewWrapper key={`${id}${size}`} className={s.product}>
             {({ isInView }) => (
               <motion.div
                 initial={{ opacity: 0, y: 30 }}
@@ -57,7 +60,21 @@ const ProductsBlock = () => {
                   }}
                   value={`${id}`}
                 >
-                  <div className={s.header}>{product.name}</div>
+                  <div className={s.header}>
+                    <div className={s.image}>
+                      <Image
+                        src={`${process.env.NEXT_PUBLIC_IMAGES_URL}${product.images[0].name}`}
+                        alt={product.name}
+                        width={100}
+                        height={100}
+                      />
+                    </div>
+                    <div className={s.info} >
+                      <div className={s.info__price}>{new Intl.NumberFormat("ru-RU").format(product.price)} ₽</div>
+                      <div className={s.info__name}>{product.name}</div>
+                      <div className={s.info__size}>размер: {size.name}</div>
+                    </div>
+                  </div>
                 </Checkbox>
                 <div className={s.footer}>
                   <HeartIcon productId={product.id} />
