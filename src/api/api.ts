ebf396e43643846e890efa2a071486f95a9b67b5
EliseevDanil <eliseev.danil.code@gmail.com>
2024-01-13 11:00:25 +0300

    #add

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,7 +5,6 @@ const $api = axios.create({
   headers: {
     Accept: "application/json",
     "X-Requested-With": "XMLHttpRequest",
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZUlkIjoyLCJpYXQiOjE3MDUwNTAyNTcsImV4cCI6MTcwNTEzNjY1N30.uLATLMzjPvzuowM7lnoVe-v16_hnfSiiRvSJkeIqJPw`,
   },
 });
 
