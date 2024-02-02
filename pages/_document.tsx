7a1c59f6bb313b15b3eaff1610aac8532a5780fc
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2024-02-02 08:31:18 +0300

    preview

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 const Document = () => (
-  <Html lang="ru">
-    <Head>
-      <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
-    </Head>
-    <body>
-      <Main />
-      <div id="portal" />
-      <NextScript />
-    </body>
-  </Html>
-);
+    <Html lang='ru'>
+        <Head>
+            <Script
+                src='https://telegram.org/js/telegram-web-app.js'
+                strategy='beforeInteractive'
+            />
+        </Head>
+        <body>
+            <Main />
+            <div id='portal' />
+            <NextScript />
+        </body>
+    </Html>
+)
 
-export default Document;
+export default Document
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,14 +1,37 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable import/no-extraneous-dependencies */
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
 const $api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    headers: {
-        Accept: 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
-        Authorization: `Bearer ${Cookies.get('token')}`
-    }
+    baseURL: process.env.NEXT_PUBLIC_API_URL
 })
+
+$api.interceptors.request.use((config) => {
+    const accessToken = Cookies.get('token')
+
+    if (config && config.headers) {
+        config.headers.Authorization = `Bearer ${
+            accessToken || process.env.NEXT_PUBLIC_API_TOKEN
+        }`
+    }
+
+    return config
+})
+
+$api.interceptors.response.use(
+    (config) => config,
+    async (error) => {
+        if (
+            error.response.status === 401 &&
+            error.config &&
+            !error.config._isRetry
+        ) {
+            error.config._isRetry = true
+
+            Cookies.remove('token')
+        }
+    }
+)
 
 export default $api
