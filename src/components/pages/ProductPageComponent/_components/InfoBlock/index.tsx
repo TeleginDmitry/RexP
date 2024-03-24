a7bd33c0ee0a9326a8bdcb3f6dae291e0a2eaf75
d1mas1k2022 <d1mas1k2KA@yandex.ru>
2024-03-24 20:46:56 +0300

    added way to share product

--- src/components/pages/ProductPageComponent/_components/InfoBlock/index.tsx
+++ src/components/pages/ProductPageComponent/_components/InfoBlock/index.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable react/jsx-no-bind */
+import { useState } from 'react'
+
 import clsx from 'clsx'
 
 import HeartIcon from '@/src/components/ui/icons/HeartIcon'
 import RootIcon from '@/src/components/ui/icons/RootIcon'
 import DefaultLink from '@/src/components/ui/links/DefaultLink'
 import { useAppSelector } from '@/src/hooks/redux-hooks/redux-hooks'
+
+import { ProductModal } from '../Modal'
 
 import s from './InfoBlock.module.scss'
 
@@ -14,6 +19,8 @@ const InfoBlock = () => {
     const activeSize = useAppSelector(
         (state) => state.filters.sizes.activeFilter
     )
+
+    const [toggleModal, setToggleModal] = useState(false)
 
     if (!activeSize) {
         return null
@@ -26,50 +33,61 @@ const InfoBlock = () => {
         ? +(productSize.price - (productSize.price * discount) / 100).toFixed(0)
         : price
 
+    function toggleModalHandler() {
+        setToggleModal((state) => !state)
+    }
+
     return (
-        <div className={s.wrapper}>
-            <div className={s.header}>
-                {productSize && (
-                    <div
-                        className={clsx(
-                            s['price-block'],
-                            newPrice.toString().length > 5 && 'flex-col'
-                        )}
-                    >
-                        <div className={s.price}>
-                            {' '}
-                            {Number.isNaN(newPrice)
-                                ? newPrice
-                                : new Intl.NumberFormat('ru-RU').format(
-                                      newPrice
-                                  )}{' '}
-                            ₽
-                        </div>
-                        {discount !== 0 && (
-                            <div className={s['old-price']}>
-                                {Number.isNaN(productSize.price)
-                                    ? price
+        <>
+            <div className={s.wrapper}>
+                <div className={s.header}>
+                    {productSize && (
+                        <div
+                            className={clsx(
+                                s['price-block'],
+                                newPrice.toString().length > 5 && 'flex-col'
+                            )}
+                        >
+                            <div className={s.price}>
+                                {' '}
+                                {Number.isNaN(newPrice)
+                                    ? newPrice
                                     : new Intl.NumberFormat('ru-RU').format(
-                                          productSize.price
+                                          newPrice
                                       )}{' '}
                                 ₽
                             </div>
-                        )}
-                    </div>
-                )}
+                            {discount !== 0 && (
+                                <div className={s['old-price']}>
+                                    {Number.isNaN(productSize.price)
+                                        ? price
+                                        : new Intl.NumberFormat('ru-RU').format(
+                                              productSize.price
+                                          )}{' '}
+                                    ₽
+                                </div>
+                            )}
+                        </div>
+                    )}
 
-                <div className={s['actions-block']}>
-                    <HeartIcon productId={id} />
-                    <DefaultLink
-                        aria-label='Ссылка на оригинальную страницу'
-                        href={`t.me/poizonrex_bot?start=product${id}`}
-                    >
-                        <RootIcon name='link' />
-                    </DefaultLink>
+                    <div className={s['actions-block']}>
+                        <HeartIcon productId={id} />
+                        <button onClick={toggleModalHandler}>
+                            <RootIcon name='link' />
+                        </button>
+                    </div>
                 </div>
+                <div className={s.name}>{name}</div>
             </div>
-            <div className={s.name}>{name}</div>
-        </div>
+
+            {toggleModal && (
+                <ProductModal
+                    isOpen={toggleModal}
+                    onClose={toggleModalHandler}
+                    productId={id}
+                />
+            )}
+        </>
     )
 }
 
---
+++ src/components/pages/ProductPageComponent/_components/Modal/index.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable react/jsx-no-bind */
+import { useState } from 'react'
+
+import { Button, Modal } from '@nextui-org/react'
+
+import Check from 'public/images/icons/check.svg'
+import Close from 'public/images/icons/close.svg'
+import Share from 'public/images/icons/share.svg'
+import Telegram from 'public/images/icons/Telegram.svg'
+import Viber from 'public/images/icons/Viber.svg'
+import Vkontakte from 'public/images/icons/Vkontakte.svg'
+import Whatsapp from 'public/images/icons/Whatsapp.svg'
+
+interface Props {
+    isOpen: boolean
+    onClose: () => void
+    productId: number | string
+}
+export const ProductModal = ({ isOpen, onClose, productId }: Props) => {
+    const [isCopied, setCopied] = useState(false)
+
+    function changeIsCopied() {
+        setCopied(true)
+    }
+
+    function copyUrl() {
+        navigator.clipboard.writeText(window.location.href)
+        changeIsCopied()
+    }
+
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <div className='fixed top-0 left-0 z-[1000000] p-4 flex justify-center items-center flex-col gap-3 w-full h-full bg-black bg-opacity-40'>
+                <div className='p-5 rounded-[20px] max-w-80 bg-white flex flex-col gap-4 w-full'>
+                    <div className='flex justify-between items-center'>
+                        <h1 className='text-base font-semibold'>Поделиться</h1>
+                        <button onClick={onClose}>
+                            <Close />
+                        </button>
+                    </div>
+                    <ul className='flex gap-2 items-center justify-between'>
+                        <a
+                            href={`https://t.me/share/url?url=t.me/poizonrex_bot?start=product${productId}`}
+                        >
+                            <Telegram />
+                        </a>
+                        <a
+                            href={`https://wa.me/?text=t.me/poizonrex_bot?start=product${productId}`}
+                        >
+                            <Whatsapp />
+                        </a>
+                        <a
+                            href={`https://vk.com/share.php?title=t.me/poizonrex_bot?start=product${productId}`}
+                        >
+                            <Vkontakte />
+                        </a>
+                        <a
+                            href={`viber://forward?text=t.me/poizonrex_bot?start=product${productId}`}
+                        >
+                            <Viber />
+                        </a>
+                    </ul>
+
+                    <Button
+                        onClick={copyUrl}
+                        className='w-full text-white font-bold bg-black !py-4 rounded-lg'
+                    >
+                        <Share /> Скопировать ссылку
+                    </Button>
+                </div>
+                {isCopied && (
+                    <div className='p-3 rounded-[12px] max-w-80 bg-white flex justify-between items-center w-full'>
+                        <p className='text-[rgba(3,164,0,1)] text-sm'>
+                            Ссылка скопирована
+                        </p>
+                        <Check />
+                    </div>
+                )}
+            </div>
+        </Modal>
+    )
+}
